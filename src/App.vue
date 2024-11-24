e8451710a8bbfb962bbae01a2ff0adfe41cd5cf2
crafzdog <240414100@fellow.lpkia.ac.id>
2024-11-24 16:05:47 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/about">About</RouterLink>
+      </li>
+    </ul>
+  </nav>
+  <div>
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+
+function increment() {
+  counter.value++;
+}
+</script>
+
+<template>
+  <h1>Home page</h1>
+  <p>Counter: {{ counter }}</p>
+
+  <button @click="increment">Click it</button>
+</template>
